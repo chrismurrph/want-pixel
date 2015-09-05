29e7903e707ff97af31822e20f1cba6db4f22ff6
chris <chris@strandz.org>
2015-09-05 10:12:32 +1000

    Updating imagez to latest version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.mikera/imagez "0.7.0"]]
+                 [net.mikera/imagez "0.8.0"]]
   :main ^:skip-aot my.issue
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
